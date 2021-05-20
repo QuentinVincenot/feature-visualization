ab6f2d9ef0c2e235620c7a3e6086174d2be97745
Quentin Vincenot <quentin.vincenot@gmail.com>
2021-05-20 19:24:14 +0200

    Implementation of convolutional filters visualization

--- feature_vizualization.py
+++ feature_vizualization.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class FeatureVisualizer:
@@ -34,3 +35,34 @@ class FeatureVisualizer:
     # Display information about the registered layers of the model
     for layer in self.layers:
       print(layer)
+
+  #--------------------------------------------------------------------------------
+  #----- Convolution filters visualization
+  #--------------------------------------------------------------------------------
+  
+  def visualize_filter(self, layer_name, filter_id):
+    # Retrieve the corresponding weights of the convolutional layer
+    weights, _ = self.model.get_layer(layer_name).get_weights()
+    # Normalize the weights of the filters to display them in grayscale
+    min_weight, max_weight = weights.min(), weights.max()
+    weights = (weights - min_weight) / (max_weight - min_weight)
+    # Compute the number of rows/columns depending on the number of channels in filters
+    MAX_COLUMNS = 8
+    if weights.shape[2] < MAX_COLUMNS:
+      nb_rows, nb_cols = 1, weights.shape[2]
+    else:
+      nb_rows, nb_cols = weights.shape[2] // MAX_COLUMNS, MAX_COLUMNS
+    # Display the different channels of the selected filter in the layer
+    _, axs = plt.subplots(nb_rows, nb_cols, figsize=(nb_cols*2, nb_rows*2))
+    plt.subplots_adjust(wspace=0.05, hspace=0.05)
+    print('Filter {}'.format(filter_id))
+    for row in range(nb_rows):
+      for col in range(nb_cols):
+        channel_id = row*MAX_COLUMNS + col
+        if nb_rows == 1:
+          axs[col].imshow(weights[:, :, channel_id, filter_id], cmap='gray')
+          axs[col].set_xticks([]) ; axs[col].set_yticks([])
+        else:
+          axs[row, col].imshow(weights[:, :, channel_id, filter_id], cmap='gray')
+          axs[row, col].set_xticks([]) ; axs[row, col].set_yticks([])
+    plt.show()
